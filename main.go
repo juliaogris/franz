f72b41cf6891a10cabcef0f47ee09e1c00e57b3a
Julia Ogris <juliao@squareup.com>
2022-02-02 15:34:25 +1100

    Work with GroupID rather than offset

--- main.go
+++ main.go
@@ -9,21 +9,25 @@ import (
 )
 
 func read(name string) {
+    ctx := context.Background()
     r := kafka.NewReader(kafka.ReaderConfig{
         Brokers:   []string{"localhost:9092"},
         Topic:     "heartbeat",
+        GroupID:   name,
         Partition: 0,
         MinBytes:  1,
         MaxBytes:  10e6,
     })
-    r.SetOffset(0)
 
     for {
-        m, err := r.ReadMessage(context.Background())
+        m, err := r.FetchMessage(ctx)
         if err != nil {
             break
         }
         fmt.Printf("%s message at offset %2d: %s = %s\n", name, m.Offset, string(m.Key), string(m.Value))
+        if err := r.CommitMessages(ctx, m); err != nil {
+            log.Fatal("failed to commit messages:", err)
+        }
     }
 
     if err := r.Close(); err != nil {
